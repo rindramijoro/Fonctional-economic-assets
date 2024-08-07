793a9df9f1c939f5ce4bbf6610b4239ccd0982c0
rindramijoro <rindramijoro@gmail.com>
2024-08-07 19:53:39 +0300

    fix: the table of the possession have been fixed, now it looks better

--- Patrimoine-Economique/src/App.jsx
+++ Patrimoine-Economique/src/App.jsx
@@ -1,69 +1,65 @@
 import Table from 'react-bootstrap/Table';
 import './App.css'
-import FloatingLabel from 'react-bootstrap/FloatingLabel';
-import Form from 'react-bootstrap/Form';
-
-
-function FormFloatingBasicExample() {
-  return (
-    <>
-      <FloatingLabel
-        controlId="floatingInput"
-        label="Email address"
-        className="mb-3"
-      >
-        <Form.Control type="email" placeholder="name@example.com" />
-      </FloatingLabel>
-      <FloatingLabel controlId="floatingPassword" label="Password">
-        <Form.Control type="password" placeholder="Password" />
-      </FloatingLabel>
-    </>
-  );
-}
+import InputGroup from 'react-bootstrap/InputGroup';
+import Button from 'react-bootstrap/Button';
 
 function App() {
   return (
     <div>
-      <Table responsive="sm">
-        <thead>
-          <tr>
-            <th>LIBELLE</th>
-            <th>VALEUR INITIALE</th>
-            <th>DEBUT</th>
-            <th>FIN</th>
-            <th>AMORTISSEMENT</th>
-            <th>VALEUR ACTUELLE</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <td>Iphone 14 pro max</td>
-            <td>829 €</td>
-            <td>2024-8-6</td>
-            <td>2026-8-6</td>
-            <td>50%</td>
-            <td>207,25 €</td>
-          </tr>
-          <tr>
-            <td>Table cell</td>
-            <td>Table cell</td>
-            <td>Table cell</td>
-            <td>Table cell</td>
-            <td>Table cell</td>
-            <td>Table cell</td>
-          </tr>
-          <tr>
-            <td>Table cell</td>
-            <td>Table cell</td>
-            <td>Table cell</td>
-            <td>Table cell</td>
-            <td>Table cell</td>
-            <td>Table cell</td>
-          </tr>
-        </tbody>
-      </Table>
+      <div className="border border-2 " style={{ width: '70vw', height: '17.8vh' }}>
+        <Table striped bordered hover>
+          <thead>
+            <h1 style={{ position: 'absolute', top: '5vh', right: '39vw', fontSize: '70px' }}> H-BUSINESS</h1>
+            <tr>
+              <th>LIBELLE</th>
+              <th>VALEUR INITIALE</th>
+              <th>DEBUT</th>
+              <th>FIN</th>
+              <th>AMORTISSEMENT</th>
+              <th>VALEUR ACTUELLE</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>Iphone 14</td>
+              <td>2500000 Ar</td>
+              <td>2024-8-7</td>
+              <td>2026-8-7</td>
+              <td>5 %</td>
+              <td>2500000 Ar</td>
+            </tr>
+            <tr>
+              <td>2</td>
+              <td>Jacob</td>
+              <td>Jacob</td>
+              <td>Jacob</td>
+              <td>Thornton</td>
+              <td>@fat</td>
+            </tr>
+            <tr>
+              <td>3</td>
+              <td>Larry the Bird</td>
+              <td>@twitter</td>
+              <td>@twitter</td>
+              <td>@twitter</td>
+              <td>@twitter</td>
+            </tr>
+
+          </tbody>
+          <h4 className='mt-5'>Date picker</h4>
+          <>
+            <InputGroup>
+              <Form.Control aria-label="Dollar amount (with dot and two decimal places)" />
+              <Button variant='primary'>Envoyer</Button>
+            </InputGroup>
+          </>
+
+        </Table>
+      </div>
     </div>
+
   );
+
 }
 
 export default App;
